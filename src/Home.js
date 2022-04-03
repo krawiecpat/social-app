eb0b2ac2e6e6b394ec31512affc16e4356c7344a
krawiecpat <krawiecpat9@gmail.com>
2022-04-03 16:58:29 +0200

    sign up

--- src/Home.js
+++ src/Home.js
@@ -36,10 +36,10 @@ function Home() {
     };
 
     axios.post('https://akademia108.pl/api/social-app/post/older-then',
-    {},
+      { "date": posts[posts.length - 1].created_at},
       axiosConfig).then((res) => {
-        setPosts(res.data.created_at.last())
-        console.log("RESPONSE RECEIVED: ", res);
+        setPosts(posts.concat(res.data))
+        console.log("Last 10 posts: ", res);
       })
       .catch((err) => {
         console.log("AXIOS ERROR: ", err);
@@ -51,11 +51,6 @@ function Home() {
   }, []
   );
 
-  useEffect(() => {
-    loadMore()
-  }, []
-  );
-
   return (
     <div className="container-post">
       {posts.map((post) => {
@@ -64,21 +59,12 @@ function Home() {
             <h3 className="user-name-post">{post.user.username}</h3>
             <p className="content-post">{post.content}</p>
             <p className="like-post"> <span className="heart">&#9829;</span> <span>{post.likes.length}</span></p>
-            <p className="data-post">{post.created_at}</p>
           </div>
         )
       })}
-      <button onClick={loadMore}>Wczytaj więcej!</button>
+      <button className="load-more" onClick={() => loadMore()}>Load more</button>
     </div>
   );
 }
 
 export default Home;
-
-
-//  1. napisz funkcję loadMore +
-// 2. w tej funkcji wywołaj zapytanie axios do adreslu post/older-than +
-// 3. pobierając dane wyślij opdowiednie body - zobacz dokumentację - ma być tam data ostatniego posta z Twojego stanu posts
-// 4. jak pobierzesz kolejne 10 postów, to dołącz je do już istniejącaj tablicy posts za pomocą metody concat
-// 5. połączone tablice dodaj ponownie do stanu za pomocą setPosts()
-// 6. funkcje loadMore wywołuj po kliknięciu w button
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,11 +1,65 @@
-import React from "react";
+import { useState } from "react";
+import "./SignUp.css";
 
-function SignUp() {
-    return (
-      <div>
-        <h2>SignUp</h2>
-      </div>
-    );
+const SignUp = (props) => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPass] = useState('')
+  const [repPass, setRepPass] = useState('')
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    console.log('Nazwa użytkownika ' + username + ' waliduje się')
+    console.log('Nazwa email ' + email + ' waliduje się')
+    console.log('Hasło ' + password + ' waliduje się')
+    console.log('Hasło 2 ' + repPass + ' waliduje się')
   }
 
-  export default SignUp;
+  return (
+    <div className="signUp">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="User name"
+          value={username}
+          onChange={(e) => { setUsername(e.target.value) }}
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => { setEmail(e.target.value) }}
+        />
+
+        <input
+          type="text"
+          name="password"
+          placeholder="Password"
+          onChange={(e) => { setPass(e.target.value) }}
+          value={password}
+        />
+
+        <input
+          type="text"
+          name="repeatPassword"
+          placeholder="Repeat password"
+          value={repPass}
+          onChange = {(e) => {setRepPass(e.target.value)}}
+        />
+
+        <button className="btn">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
+
+// 1.walidacja jednego
+// bez bialyach znakow i wymagane 4 litery
+// nie pozwalaj wyslac form 
